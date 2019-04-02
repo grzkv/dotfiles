51e1a68be7e9a8681ab56c58a9213431725d0dd7
Roman Grytskiv <roman.grytskiv@gmail.com>
2019-04-02 16:42:50 +0200

    upd

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -2,13 +2,24 @@
 
 sudo add-apt-repository ppa:mozillateam/firefox-next
 sudo add-apt-repository ppa:neovim-ppa/stable
-udo add-apt-repository ppa:jonathonf/vim
+sudo add-apt-repository ppa:jonathonf/vim
 
 sudo apt-get update
 
-sudo apt-get install zsh
-sudo apt-get install wget curl glances nnn
+sudo apt-get install zsh git
+sudo apt-get install wget curl glances nnn tmux net-tools zsh-syntax-highlighting
 sudo apt-get install firefox
+
+# FZF
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+$HOME/.fzf/install
+
+# DOTFILES
+git clone github.com/grzkv/dotfiles $HOME/dotfiles
+cd $HOME/dotfiles
+make
+
+# ZSH
 
 # VIM
 sudo apt-get install vim
