6187cebb9aeb2aae786fc5e37b88f5a72a9af315
Roman Grytskiv <roman.grytskiv@gmail.com>
2019-04-04 11:09:00 +0200

    upg

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usb/bin/env bash
 
 sudo add-apt-repository ppa:mozillateam/firefox-next
 sudo add-apt-repository ppa:neovim-ppa/stable
@@ -7,38 +7,38 @@ sudo add-apt-repository ppa:jonathonf/vim
 sudo apt-get update
 
 sudo apt-get install zsh git
-sudo apt-get install wget curl glances nnn tmux net-tools zsh-syntax-highlighting tldr tig ripgrep
+sudo apt-get install wget curl glances nnn tmux net-tools zsh-syntax-highlighting tldr tig ripgrep shellcheck
 sudo apt-get install vim nvim
 sudo apt-get install firefox
 
 # FZF
-git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
-$HOME/.fzf/install
+git clone --depth 1 'https://github.com/junegunn/fzf.git' .fzf
+.fzf/install
 
 # JUMP
-wget https://github.com/gsamokovarov/jump/releases/download/v0.22.0/jump_0.22.0_amd64.deb
-sudo dpkg -i jump_0.22.0_amd64.deb
+wget 'https://github.com/gsamokovarov/jump/releases/download/v0.22.0/jump_0.22.0_amd64.deb'
+sudo dpkg -i 'jump_0.22.0_amd64.deb'
 
 # BUILD ST
 install_st() {
-    st='st-0.8.2'
-    mkdir /tmp/st
-    wget "https://dl.suckless.org/st/$st.tar.gz" /tmp/st
-    tar -xzf "/tmp/st/$st.tar.gz" -C "/tmp/st/$st"
-    cd "/tmp/st/$st"
-    stsolpatch=st-solarized-light-20190306-ed68fe7
+    st="st-0.8.2"
+    wget "https://dl.suckless.org/st/$st.tar.gz"
+    tar -xzf "$st.tar.gz" -C "$st"
+    cd "$st" || return
+    stsolpatch="st-solarized-light-20190306-ed68fe7"
     wget "https://st.suckless.org/patches/solarized/$stsolpatch.diff"
-    git apply $stsolpatch.diff
+    git apply "$stsolpatch.diff"
     sudo make clean install
-    cd ~
+    cd .. || exit 1
 }
 
 install_st
 
 # DOTFILES
-git clone github.com/grzkv/dotfiles $HOME/dotfiles
-cd $HOME/dotfiles
+git clone github.com/grzkv/dotfiles dotfiles
+cd dotfiles || echo "could not go to dotfiles dir"
 make
 
-# SWITCH SELLS
-chsh -s $(which zsh)
+# SWITCH SHELLS
+chsh -s "$(command -v zsh)"
+
