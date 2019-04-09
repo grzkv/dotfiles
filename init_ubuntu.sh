2c7124af75f8c380f27c7bba403de35fb12da47f
Roman Grytskiv <roman.grytskiv@gmail.com>
2019-04-09 12:15:46 +0200

    error handling, env propagation

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -1,24 +1,30 @@
 #!/usr/bin/env bash
 
+err () {
+    printf "\\n>>> ERROR: %s\\n" "$@" >&2
+}
+
 add_ppas ()
 {
-    sudo add-apt-repository -y ppa:mozillateam/firefox-next || printf "ERROR: failed to add firefox PPA"
-    sudo add-apt-repository -y ppa:neovim-ppa/stable || printf "ERROR: failed to add neovim PPA"
-    sudo add-apt-repository -y ppa:jonathonf/vim || printf "ERROR: failed to add vim PPA"
-    # sudo apt-get install apt-transport-https ca-certificates gnupg-agent software-properties-common
-    # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" || printf "ERROR: failed to add docker PPA"
+    sudo -E add-apt-repository -y ppa:mozillateam/firefox-next || err "failed to add firefox PPA"
+    sudo -E add-apt-repository -y ppa:neovim-ppa/stable || err "failed to add neovim PPA"
+    sudo -E add-apt-repository -y ppa:jonathonf/vim || err "failed to add vim PPA"
+    # sudo -E apt-get install apt-transport-https ca-certificates gnupg-agent software-properties-common
+    # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo -E apt-key add -
+    sudo -E add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" || err "failed to add docker PPA"
+    sudo -E add-apt-repository -y ppa:longsleep/golang-backports || err "failed to add golang PPA"
 
-    sudo apt-get -y update
+    sudo -E apt-get -y update
 }
 add_ppas
 
-sudo apt-get -y install zsh git wget curl glances nnn tmux net-tools zsh-syntax-highlighting tldr tig ripgrep shellcheck icdiff peco fpp vim neovim firefox thunderbird || printf "ERROR: installing packages failed"
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io || printf "ERROR: Installing docker failed"
+sudo -E apt-get -y install zsh git wget curl glances nnn tmux net-tools zsh-syntax-highlighting tldr tig ripgrep shellcheck icdiff peco fpp vim neovim firefox thunderbird || err "installing packages failed"
+sudo -E apt-get -y install golang-go || err "while installing dev tools"
+sudo -E apt-get -y install docker-ce docker-ce-cli containerd.io || err "Installing docker failed"
 
 install_fzf ()
 {
-    git clone --depth 1 'https://github.com/junegunn/fzf.git' ~/.fzf || { printf "ERROR: cloning fzf repo failed"; return; }
+    git clone --depth 1 'https://github.com/junegunn/fzf.git' ~/.fzf || { err "cloning fzf repo failed"; return; }
     ~/.fzf/install
 }
 install_fzf
@@ -28,8 +34,8 @@ trap rm -rf "$tmpdir"
 
 install_jump ()
 {
-    wget 'https://github.com/gsamokovarov/jump/releases/download/v0.22.0/jump_0.22.0_amd64.deb' -P "$tmpdir" || { printf "ERROR: getting jump failed"; return; }
-    sudo dpkg -i "$tmpdir/jump_0.22.0_amd64.deb" || printf "ERROR: installing jump failed"
+    wget 'https://github.com/gsamokovarov/jump/releases/download/v0.22.0/jump_0.22.0_amd64.deb' -P "$tmpdir" || { err "getting jump failed"; return; }
+    sudo -E dpkg -i "$tmpdir/jump_0.22.0_amd64.deb" || err "installing jump failed"
 }
 install_jump
 
@@ -42,7 +48,7 @@ install_jump
 #     stsolpatch="st-solarized-light-20190306-ed68fe7"
 #     wget "https://st.suckless.org/patches/solarized/$stsolpatch.diff"
 #     git apply "$stsolpatch.diff"
-#     sudo make clean install
+#     sudo -E make clean install
 #     cd .. || exit 1
 # }
 # 
@@ -50,12 +56,12 @@ install_jump
 
 setup_dotfiles ()
 {
-    git clone https://github.com/grzkv/dotfiles ~/dotfiles || { printf "ERROR while cloning dotfiles"; return; }
+    git clone https://github.com/grzkv/dotfiles ~/dotfiles || { err "while cloning dotfiles"; return; }
 
-    ln -s ~/dotfiles/.vimrc ~/.vimrc || printf "ERROR: could not symlink vimrc"
-    ln -s ~/dotfiles/.zshrc ~/.zshrc || printf "ERROR: could not symlink zshrc"
-    ln -s ~/dotfiles/.gitconfig ~/.gitconfig || printf "ERROR: could not symlink gitconfig"
-    ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf || printf "ERROR: could not symlink tmux.conf"
+    ln -s ~/dotfiles/.vimrc ~/.vimrc || err "could not symlink vimrc"
+    ln -s ~/dotfiles/.zshrc ~/.zshrc || err "could not symlink zshrc"
+    ln -s ~/dotfiles/.gitconfig ~/.gitconfig || err "could not symlink gitconfig"
+    ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf || err "could not symlink tmux.conf"
 }
 setup_dotfiles
 
@@ -67,7 +73,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-sy
 
 install_plug ()
 {
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim || { printf "ERROR getting vim-plug"; return; }
-    vim +PlugInstall +qall || printf "ERROR installing vim plugins"
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim || { err "getting vim-plug"; return; }
+    vim +PlugInstall +qall || err "installing vim plugins"
 }
 install_plug
